a7f45524b1adc16f00fac2cdce247057ef51ff88
James Moore <james@restphone.com>
2018-04-13 09:20:12 -0700

    Added repro for xUnit bug

---
+++ MSTestProj/Tests.fs
@@ -0,0 +1,11 @@
+namespace MSTestProj
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
